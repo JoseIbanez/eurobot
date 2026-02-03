8c8dfe404ad1d93c9971c20657fffaceef08c632
Jose Ibanez (rpi) <ibanez.j@gmail.com>
2026-02-03 19:53:30 +0100

    fix(servo): test

--- servo-main.py
+++ servo-main.py
@@ -1,24 +1,29 @@
 import time
 from adafruit_servokit import ServoKit 
 
-pca = ServoKit(channels=16)
+pca = ServoKit(channels=16, address=0x41)
 
 
 pca.servo[0].set_pulse_width_range(500, 2400)
 pca.servo[15].set_pulse_width_range(500, 2400)
 time.sleep(2)
 
-pca.servo[0].angle = 180
-pca.servo[15].angle = 90
-time.sleep(1)
+
+for i in [0,1,2]:
+
+   print("pos 1")
+   pca.servo[0].angle =   90
+   pca.servo[15].angle =  0
+   time.sleep(0.5)
+
+   print("pos 2")
+   pca.servo[0].angle =   0
+   pca.servo[15].angle = 100
+   time.sleep(0.5)
 
 
-pca.servo[0].angle = 90
-pca.servo[15].angle = 0
-time.sleep(1)
-
-
-pca.servo[0].angle = 0
-pca.servo[15].angle = 0
+print("pos 3")
+pca.servo[0].angle =  90
+pca.servo[15].angle =  0
 time.sleep(1)
 
